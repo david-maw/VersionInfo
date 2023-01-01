c192232c5ef8d091cf30c5504af9b3fb3f9fbbc5
David Maw <david@maw.name>
2022-12-31 16:38:39 -0800

    Add project files.

---
+++ VersionInfo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VersionInfo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ VersionInfo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VersionInfo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
